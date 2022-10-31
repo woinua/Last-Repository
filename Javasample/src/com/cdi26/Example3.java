5f25dbf10c39f006944e6ecdac86b30275566ec8
woinua <woinialem@gmail.com>
2022-10-31 12:26:42 -0400

    best eclipse commit

---
+++ Javasample/src/com/cdi26/Example3.java
@@ -0,0 +1,13 @@
+package com.cdi26;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My name is Woini");
+		System.out.println("My name is Woini");
+
+	}
+
+}
---
+++ Javasample/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author test
+ *
+ */
+module Javasample {
+}
